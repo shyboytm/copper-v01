73aa0c284c3a2af70388f88333f86248df50c269
cortesarts <dennis.cortes@satchelhealth.com>
2016-07-27 17:56:31 -0500

    Deletes helpers.scss file and merges into the main scss file. Also fixes issue #11

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,13 +6,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', 'helpers' function() {
+gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'))
 });
 
-//Watch task
+// Watch task
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['styles']);
 });

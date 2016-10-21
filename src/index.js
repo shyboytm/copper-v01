59451072d907ac0bfd8e0daaafee47fa2ecf3f8a
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-21 17:03:45 -0500

    Refreshing app code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Home from './components/Home/index.jsx';
-import About from './components/About/index.jsx';
-
 import './index.css';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route path="about" component={About} />
-    </Route>
-  </Router>,
+  <App />,
   document.getElementById('root')
 );

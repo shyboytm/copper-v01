f4fd29576f593a0f1f0048e69cc992d13d770340
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-25 19:31:03 -0400

    Reorganizes layout of banner

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,6 +7,18 @@ class Home extends Component {
     return (
       <div id="Home">
         <Sidebar />
+        <div id="HomePage" className="mv0 fr w-80-l w-100">
+          <div id="Banner" className="bg-black pv5 ph5">
+            <div className="w-60-l w-80-m w-90">
+              <h1 className="mt0 fw5 white">Curated Resources for Humans</h1>
+              <p className="mb0 lh-copy white-80">
+                We are a team of two that create simple, beautiful digital experiences
+                and resources with the intention of organizing and curating some of the
+                world's most popular online topics.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

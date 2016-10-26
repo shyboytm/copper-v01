97f8e0f5a0403aacf14a17652bcab5f3c160087a
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-25 20:59:59 -0400

    Finishes banner for homepage

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -8,10 +8,12 @@ class Home extends Component {
       <div id="Home">
         <Sidebar />
         <div id="HomePage" className="mv0 fr w-80-l w-100">
-          <div id="Banner" className="bg-black pv5 ph5">
-            <div className="w-60-l w-80-m w-90">
-              <h1 className="mt0 fw5 white">Curated Resources for Humans</h1>
-              <p className="mb0 lh-copy white-80">
+          <div id="Banner" className="bg-black ph6-l pv6-l pa5-m pa4">
+            <div className="w-100">
+              <h1 className="mt0 fw4 f2-ns f4 white t-tracking lh-title">
+                Curated Resources for Humans
+              </h1>
+              <p className="mb0 lh-copy measure white-70">
                 We are a team of two that create simple, beautiful digital experiences
                 and resources with the intention of organizing and curating some of the
                 world's most popular online topics.

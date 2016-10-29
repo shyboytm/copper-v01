bff698a3fba07c71ce8a21214cfc57b99c1e12e5
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-29 12:07:04 -0400

    Removes unnecessary Contact component

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-class Contact extends Component {
+class ContactUs extends Component {
   render() {
 
     return (
-      <div id="Contact" className="w-100 pv5 ph4 center tc bg-near-white">
+      <div id="ContactUs" className="w-100 pv5 ph4 center tc bg-near-white">
         <h2 className="fw5 f3-ns f4 black lh-title">Contact Us</h2>
         <p className="mb4 lh-copy black-60">We’re always open for sponsors, feedback, or just to say to hey!</p>
         <a className="f6 link dim br2 ph4 pv3 mb2 dib white bg-black" href="mailto:contact@usequarry.com">Email Us</a>
@@ -13,4 +13,4 @@ class Contact extends Component {
   }
 }
 
-export default Contact;
+export default ContactUs;

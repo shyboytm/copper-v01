27c90f0243ee1465f40439ce071b31ad3a3a0b0d
cortesarts <dennis.cortes@satchelhealth.com>
2016-10-30 13:35:44 -0400

    Adds Transparency section to About page

--- src/components/pages/About/index.jsx
+++ src/components/pages/About/index.jsx
@@ -23,6 +23,30 @@ class About extends Component {
               We focus on making things that help others."
             backgroundColor="bg-blue" />
 
+          <div id="Transparency">
+            <div className="ph6-l pv5-l pa5-m pa4">
+              <h2 className="mb4 fw5 f3-ns f4 black lh-title">Transparency</h2>
+              <div className="cf">
+                <dl className="db dib-l w-auto-l lh-title mr6-l">
+                  <dd className="f6 f5-ns ml0 blue">Projects</dd>
+                  <dd className="f3 f2-ns b ml0">7</dd>
+                </dl>
+                <dl className="db dib-l w-auto-l lh-title mr6-l">
+                  <dd className="f6 f5-ns ml0 light-purple">Hrs worked</dd>
+                  <dd className="f3 f2-ns b ml0">620~</dd>
+                </dl>
+                <dl className="db dib-l w-auto-l lh-title mr6-l">
+                  <dd className="f6 f5-ns ml0 green">Hrs per week</dd>
+                  <dd className="f3 f2-ns b ml0">20</dd>
+                </dl>
+                <dl className="db dib-l w-auto-l lh-title mr6-l">
+                  <dd className="f6 f5-ns ml0 light-red">Monthly cost</dd>
+                  <dd className="f3 f2-ns b ml0">$120~</dd>
+                </dl>
+              </div>
+            </div>
+          </div>
+
           <ContactUs />
           <Footer />
 
